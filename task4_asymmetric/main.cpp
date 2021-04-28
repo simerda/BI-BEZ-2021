3d3cbce6bad55c665e56a65691fcdea88971ae7a
Jan Šimerda <simerda.jan@gmail.com>
2021-04-28 23:18:55 +0200

    feat(task4): Adding decryptor.

--- task4_asymmetric/main.cpp
+++ task4_asymmetric/main.cpp
@@ -27,17 +27,17 @@ public:
         // set file buffer size as next greater or equal number that is divisible by cipherStr block size
         fileBufferSize = (int) ceil( // NOLINT(cppcoreguidelines-narrowing-conversions)
                 MINIMUM_FILE_BUFFER_SIZE / (double) EVP_CIPHER_block_size(cipher)
-                ) * MINIMUM_FILE_BUFFER_SIZE;
+                ) * EVP_CIPHER_block_size(cipher);
 
         FILE *pubKeyFile = fopen(publicKeyPath.c_str(), "rt");
         if (!pubKeyFile) {
-            throw runtime_error("Could not open file with private key.");
+            throw runtime_error("Could not open file with public key.");
         }
 
         publicKey = PEM_read_PUBKEY(pubKeyFile, nullptr, nullptr, nullptr);
         fclose(pubKeyFile);
         if (!publicKey) {
-            throw invalid_argument("Could not read private key.");
+            throw invalid_argument("Could not read public key.");
         }
         encryptedKeyBufferSize = EVP_PKEY_size(publicKey);
         encryptedKey = new unsigned char[encryptedKeyBufferSize];
@@ -93,8 +93,11 @@ public:
 
         int nid = EVP_CIPHER_nid(cipher);
         outputFile.write(reinterpret_cast<char *>(&nid), sizeof(nid));
+        outputFile.write(reinterpret_cast<char *>(&encryptedKeyBufferSize), sizeof(encryptedKeyBufferSize));
         outputFile.write(reinterpret_cast<char *>(encryptedKey), encryptedKeyBufferSize);
-        outputFile.write(reinterpret_cast<char *>(iv), ivLength);
+        if(iv != nullptr){
+            outputFile.write(reinterpret_cast<char *>(iv), ivLength);
+        }
 
         inBuffer = new unsigned char[fileBufferSize];
         outBuffer = new unsigned char[fileBufferSize];
@@ -105,7 +108,7 @@ public:
                 break;
             }
 
-            if(EVP_EncryptUpdate(context, outBuffer, &fileBufferSize, inBuffer, bytesRead) != 1){
+            if(EVP_SealUpdate(context, outBuffer, &fileBufferSize, inBuffer, bytesRead) != 1){
                 throw runtime_error("Failed to encrypt.");
             }
 
@@ -140,10 +143,138 @@ private:
     unsigned char *outBuffer = nullptr;
 };
 
+
+class SealDecryptor {
+public:
+    SealDecryptor(const string &privateKeyPath, const string &sealedPath)
+    {
+        EVP_PKEY *privateKey = nullptr;
+        FILE *privKeyFile = fopen(privateKeyPath.c_str(), "rt");
+        if (!privKeyFile) {
+            throw runtime_error("Could not open file with private key.");
+        }
+        privateKey = PEM_read_PrivateKey(privKeyFile, nullptr, nullptr, nullptr);
+        fclose(privKeyFile);
+        if (!privateKey) {
+            throw invalid_argument("Could not read private key.");
+        }
+
+        inputFile.open(sealedPath, ios_base::in | ios_base::binary);
+        int nid;
+        inputFile.read(reinterpret_cast<char *>(&nid), sizeof(nid));
+
+        if(! inputFile){
+            EVP_PKEY_free(privateKey);
+            throw invalid_argument("Failed to read sealed file.");
+        }
+
+        const EVP_CIPHER *cipher = EVP_get_cipherbynid(nid);
+        if(!cipher){
+            EVP_PKEY_free(privateKey);
+            throw invalid_argument("Provided sealed file is invalid.");
+        }
+
+        int encryptedKeyLength;
+        inputFile.read(reinterpret_cast<char *>(&encryptedKeyLength), sizeof(encryptedKeyLength));
+        if(encryptedKeyLength <= 0 || encryptedKeyLength >= 10000){
+            EVP_PKEY_free(privateKey);
+            throw invalid_argument("Provided sealed file is invalid.");
+        }
+        auto encryptedKey = new unsigned char[encryptedKeyLength];
+        inputFile.read(reinterpret_cast<char *>(encryptedKey), encryptedKeyLength);
+
+        int ivLength = EVP_CIPHER_iv_length(cipher);
+        unsigned char *iv = nullptr;
+
+        if(ivLength > 0){
+            iv = new unsigned char[ivLength];
+            inputFile.read(reinterpret_cast<char *>(iv), ivLength);
+        }
+
+        if(! inputFile){
+            EVP_PKEY_free(privateKey);
+            throw runtime_error("Provided sealed file is invalid.");
+        }
+
+        context = EVP_CIPHER_CTX_new();
+        int outcome = EVP_OpenInit(context, cipher, encryptedKey, encryptedKeyLength, iv, privateKey);
+        EVP_PKEY_free(privateKey);
+
+        if(outcome != 1){
+            throw runtime_error("Failed to initialize EVP open.");
+        }
+
+        // set file buffer size as next greater or equal number that is divisible by cipherStr block size
+        fileBufferSize = (int) ceil( // NOLINT(cppcoreguidelines-narrowing-conversions)
+                MINIMUM_FILE_BUFFER_SIZE / (double) EVP_CIPHER_block_size(cipher)
+        ) * EVP_CIPHER_block_size(cipher);
+        delete[] iv;
+        delete[] encryptedKey;
+    }
+
+    ~SealDecryptor()
+    {
+        EVP_CIPHER_CTX_free(context);
+        delete[] inBuffer;
+        delete[] outBuffer;
+    }
+
+
+    void decryptFile(const string &path)
+    {
+        ofstream outputFile;
+        outputFile.open(path, ios_base::out | ios_base::binary);
+        if(! outputFile){
+            throw runtime_error("Failed to open output file.");
+        }
+
+        inBuffer = new unsigned char[fileBufferSize];
+        outBuffer = new unsigned char[fileBufferSize];
+        do{
+            inputFile.read(reinterpret_cast<char *>(inBuffer), fileBufferSize);
+            streamsize bytesRead = inputFile.gcount();
+            if(bytesRead <= 0){
+                break;
+            }
+
+            if(EVP_OpenUpdate(context, outBuffer, &fileBufferSize, inBuffer, bytesRead) != 1){
+                throw runtime_error("Failed to decrypt.");
+            }
+
+            outputFile.write(reinterpret_cast<char *>(outBuffer), fileBufferSize);
+        }while(inputFile);
+
+        if(! inputFile.eof()){
+            throw runtime_error("Failed reading from file.");
+        }
+
+        int written;
+        if(EVP_OpenFinal(context, outBuffer, &written) != 1){
+            throw runtime_error("Failed to decrypt.");
+        }
+
+        outputFile.write(reinterpret_cast<char *>(outBuffer), written);
+        if(! outputFile){
+            throw runtime_error("Failed to write decrypted data.");
+        }
+    }
+
+
+private:
+    ifstream inputFile;
+    EVP_CIPHER_CTX *context = nullptr;
+    int fileBufferSize;
+    unsigned char *inBuffer;
+    unsigned char *outBuffer;
+};
+
 int main()
 {
-    SealEncryptor encryptor("aes-128-cbc", "/home/simerjan/projects/cvut/BEZ/repo/task4_asymmetric/pubkey.pem");
-    encryptor.encryptFile("/home/simerjan/Downloads/vzor_platby.csv");
+    SealEncryptor encryptor("aria-128-cfb8", "/home/simerjan/projects/cvut/BEZ/repo/task4_asymmetric/pubkey.pem");
+    encryptor.encryptFile("/home/simerjan/Downloads/mariadb.sql");
+
+    SealDecryptor decryptor("/home/simerjan/projects/cvut/BEZ/repo/task4_asymmetric/privkey.pem", "/home/simerjan/Downloads/mariadb.sql_seal");
+    decryptor.decryptFile("/home/simerjan/Downloads/mariadb.sql_unsealed");
 
     return 0;
 }
